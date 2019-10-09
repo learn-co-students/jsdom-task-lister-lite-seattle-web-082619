8602c6072bfbc83532dbb0c25d60b09b58fbc9f1
Krista Loven <krista.loven.87@gmail.com>
2019-10-09 09:56:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  toDoList();
 });
+
+function toDoList() {
+  const form = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    list.appendChild(li); 
+  });
+}
